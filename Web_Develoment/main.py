5603ea6aedeedf2276af7e8077f548068694b6c0
Tayaba Anwer <100337978+Tayyba27@users.noreply.github.com>
2023-12-03 18:27:29 +0500

    Add files via upload

--- Web_Develoment/main.py
+++ Web_Develoment/main.py
@@ -1,33 +1,67 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
 
-URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+chrome_driver_path = "YOUR CHROME DRIVER PATH"
+driver = webdriver.Chrome(chrome_driver_path)
+driver.get("http://orteil.dashnet.org/experiments/cookie/")
 
-# Write your code below this line 👇
+#Get cookie to click on.
+cookie = driver.find_element_by_id("cookie")
 
-response = requests.get(URL)
-website_html = response.text
+#Get upgrade item ids.
+items = driver.find_elements_by_css_selector("#store div")
+item_ids = [item.get_attribute("id") for item in items]
 
-soup = BeautifulSoup(website_html, "html.parser")
+timeout = time.time() + 5
+five_min = time.time() + 60*5 # 5minutes
 
-all_movies = soup.find_all(name="h3", class_="title")
+while True:
+    cookie.click()
 
-movie_titles = [movie.getText() for movie in all_movies]
-movies = movie_titles[::-1]
+    #Every 5 seconds:
+    if time.time() > timeout:
 
-with open("movies.txt", mode="w") as file:
-    for movie in movies:
-        file.write(f"{movie}\n")
+        #Get all upgrade <b> tags
+        all_prices = driver.find_elements_by_css_selector("#store b")
+        item_prices = []
 
+        #Convert <b> text into an integer price.
+        for price in all_prices:
+            element_text = price.text
+            if element_text != "":
+                cost = int(element_text.split("-")[1].strip().replace(",", ""))
+                item_prices.append(cost)
 
-'''
-FAQ: Empire's website has changed!
+        #Create dictionary of store items and prices
+        cookie_upgrades = {}
+        for n in range(len(item_prices)):
+            cookie_upgrades[item_prices[n]] = item_ids[n]
 
-When this lesson was created, I used this URL for the project: 
-URL = "https://www.empireonline.com/movies/features/best-movies-2/"
+        #Get current cookie count
+        money_element = driver.find_element_by_id("money").text
+        if "," in money_element:
+            money_element = money_element.replace(",", "")
+        cookie_count = int(money_element)
 
-However, Empire has since changed their website. You can see this when you inspect the movie title elements. 
-You'll see that the h3 with the class "title" is no longer there. 
-To use exactly the same code as per the solution, we can use a cached version of the website from the Internet Archive's Wayback Machine.
+        #Find upgrades that we can currently afford
+        affordable_upgrades = {}
+        for cost, id in cookie_upgrades.items():
+            if cookie_count > cost:
+                 affordable_upgrades[cost] = id
 
-'''
+        #Purchase the most expensive affordable upgrade
+        highest_price_affordable_upgrade = max(affordable_upgrades)
+        print(highest_price_affordable_upgrade)
+        to_purchase_id = affordable_upgrades[highest_price_affordable_upgrade]
+
+        driver.find_element_by_id(to_purchase_id).click()
+        
+        #Add another 5 seconds until the next check
+        timeout = time.time() + 5
+
+    #After 5 minutes stop the bot and check the cookies per second count.
+    if time.time() > five_min:
+        cookie_per_s = driver.find_element_by_id("cps").text
+        print(cookie_per_s)
+        break
+
