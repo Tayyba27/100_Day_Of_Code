92a58bd6ec9f9c035fb0ffbc080800b9fac9a01f
Tayaba Anwer <100337978+Tayyba27@users.noreply.github.com>
2024-01-04 22:05:28 +0500

    Add files via upload

--- Web_Develoment/main.py
+++ Web_Develoment/main.py
@@ -1,52 +1,31 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
+from flask import Flask, render_template
+from post import Post
+import requests
 
-PROMISED_DOWN = 150
-PROMISED_UP = 10
-CHROME_DRIVER_PATH = "YOUR CHROME DRIVER PATH"
-TWITTER_EMAIL = "YOUR TWITTER EMAIL"
-TWITTER_PASSWORD = "YOUR TWITTER PASSWORD"
+posts = requests.get("https://api.npoint.io/5abcca6f4e39b4955965").json()
+post_objects = []
+for post in posts:
+    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
+    post_objects.append(post_obj)
 
-
-class InternetSpeedTwitterBot:
-    def __init__(self, driver_path):
-        self.driver = webdriver.Chrome(executable_path=driver_path)
-        self.up = 0
-        self.down = 0
-
-    def get_internet_speed(self):
-        self.driver.get("https://www.speedtest.net/")
-        # accept_button = self.driver.find_element_by_id("_evidon-banner-acceptbutton")
-        # accept_button.click()
-        # time.sleep(3)
-
-        go_button = self.driver.find_element_by_css_selector(".start-button a")
-        go_button.click()
-        time.sleep(60)
-        self.up = self.driver.find_element_by_xpath('//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
-        self.down = self.driver.find_element_by_xpath('//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[3]/div/div[2]/span').text
-
-    def tweet_at_provider(self):
-        self.driver.get("https://twitter.com/login")
-        time.sleep(2)
-        email = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div[1]/label/div/div[2]/div/input')
-        password = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div[2]/label/div/div[2]/div/input')
-        email.send_keys(TWITTER_EMAIL)
-        password.send_keys(TWITTER_PASSWORD)
-        time.sleep(2)
-        password.send_keys(Keys.ENTER)
-        time.sleep(5)
-        tweet_compose = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/div[1]/div/div/div/div[2]/div/div/div/div')
-        tweet = f"Hey Internet Provider, why is my internet speed {self.down}down/{self.up}up when I pay for {PROMISED_DOWN}down/{PROMISED_UP}up?"
-        tweet_compose.send_keys(tweet)
-        time.sleep(3)
-        tweet_button = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[4]/div/div/div[2]/div[3]')
-        tweet_button.click()
-        time.sleep(2)
-        self.driver.quit()
+app = Flask(__name__)
 
 
-bot = InternetSpeedTwitterBot(CHROME_DRIVER_PATH)
-bot.get_internet_speed()
-bot.tweet_at_provider()
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html", all_posts=post_objects)
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in post_objects:
+        if blog_post.id == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
