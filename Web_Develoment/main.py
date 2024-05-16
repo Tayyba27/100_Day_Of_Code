6bd43e22d5df08074dbaf3c04cc71cf3c0d43181
Tayaba Anwer <100337978+Tayyba27@users.noreply.github.com>
2024-05-16 10:37:35 +0500

    Add files via upload

--- Web_Develoment/main.py
+++ Web_Develoment/main.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Float
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import requests
 
 '''
 Red underlines? Install the required packages first: 
@@ -16,87 +21,114 @@ pip3 install -r requirements.txt
 This will install the packages from requirements.txt for this project.
 '''
 
+
+MOVIE_DB_API_KEY = "USE_YOUR_OWN_CODE"
+MOVIE_DB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie"
+MOVIE_DB_INFO_URL = "https://api.themoviedb.org/3/movie"
+MOVIE_DB_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap5(app)
 
-# CREATE DATABASE
-
-
+# CREATE DB
 class Base(DeclarativeBase):
     pass
-
-
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///books.db"
-# Create the extension
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
 db = SQLAlchemy(model_class=Base)
-# initialise the app with the extension
 db.init_app(app)
 
-
 # CREATE TABLE
-class Book(db.Model):
+class Movie(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    title: Mapped[str] = mapped_column(
-        String(250), unique=True, nullable=False)
-    author: Mapped[str] = mapped_column(String(250), nullable=False)
-    rating: Mapped[float] = mapped_column(Float, nullable=False)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    year: Mapped[int] = mapped_column(Integer, nullable=False)
+    description: Mapped[str] = mapped_column(String(500), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=True)
+    ranking: Mapped[int] = mapped_column(Integer, nullable=True)
+    review: Mapped[str] = mapped_column(String(250), nullable=True)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
 
 
-# Create table schema in the database. Requires application context.
 with app.app_context():
     db.create_all()
 
 
-@app.route('/')
+class FindMovieForm(FlaskForm):
+    title = StringField("Movie Title", validators=[DataRequired()])
+    submit = SubmitField("Add Movie")
+
+
+class RateMovieForm(FlaskForm):
+    rating = StringField("Your Rating Out of 10 e.g. 7.5")
+    review = StringField("Your Review")
+    submit = SubmitField("Done")
+
+
+@app.route("/")
 def home():
-    # READ ALL RECORDS
-    # Construct a query to select from the database. Returns the rows in the database
-    result = db.session.execute(db.select(Book).order_by(Book.title))
-    # Use .scalars() to get the elements rather than entire rows from the database
-    all_books = result.scalars().all()
-    return render_template("index.html", books=all_books)
+    result = db.session.execute(db.select(Movie).order_by(Movie.rating))
+    all_movies = result.scalars().all()  # convert ScalarResult to Python List
+
+    for i in range(len(all_movies)):
+        all_movies[i].ranking = len(all_movies) - i
+    db.session.commit()
+
+    return render_template("index.html", movies=all_movies)
 
 
 @app.route("/add", methods=["GET", "POST"])
-def add():
-    if request.method == "POST":
-        # CREATE RECORD
-        new_book = Book(
-            title=request.form["title"],
-            author=request.form["author"],
-            rating=request.form["rating"]
+def add_movie():
+    form = FindMovieForm()
+    if form.validate_on_submit():
+        movie_title = form.title.data
+        response = requests.get(MOVIE_DB_SEARCH_URL, params={
+                                "api_key": MOVIE_DB_API_KEY, "query": movie_title})
+        data = response.json()["results"]
+        return render_template("select.html", options=data)
+    return render_template("add.html", form=form)
+
+
+@app.route("/find")
+def find_movie():
+    movie_api_id = request.args.get("id")
+    if movie_api_id:
+        movie_api_url = f"{MOVIE_DB_INFO_URL}/{movie_api_id}"
+        response = requests.get(movie_api_url, params={
+                                "api_key": MOVIE_DB_API_KEY, "language": "en-US"})
+        data = response.json()
+        new_movie = Movie(
+            title=data["title"],
+            year=data["release_date"].split("-")[0],
+            img_url=f"{MOVIE_DB_IMAGE_URL}{data['poster_path']}",
+            description=data["overview"]
         )
-        db.session.add(new_book)
+        db.session.add(new_movie)
         db.session.commit()
-        return redirect(url_for('home'))
-    return render_template("add.html")
+        return redirect(url_for("rate_movie", id=new_movie.id))
 
 
 @app.route("/edit", methods=["GET", "POST"])
-def edit():
-    if request.method == "POST":
-        # UPDATE RECORD
-        book_id = request.form["id"]
-        book_to_update = db.get_or_404(Book, book_id)
-        book_to_update.rating = request.form["rating"]
+def rate_movie():
+    form = RateMovieForm()
+    movie_id = request.args.get("id")
+    movie = db.get_or_404(Movie, movie_id)
+    if form.validate_on_submit():
+        movie.rating = float(form.rating.data)
+        movie.review = form.review.data
         db.session.commit()
         return redirect(url_for('home'))
-    book_id = request.args.get('id')
-    book_selected = db.get_or_404(Book, book_id)
-    return render_template("edit_rating.html", book=book_selected)
+    return render_template("edit.html", movie=movie, form=form)
 
 
 @app.route("/delete")
-def delete():
-    book_id = request.args.get('id')
-
-    # DELETE A RECORD BY ID
-    book_to_delete = db.get_or_404(Book, book_id)
-    # Alternative way to select the book to delete.
-    # book_to_delete = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
-    db.session.delete(book_to_delete)
+def delete_movie():
+    movie_id = request.args.get("id")
+    movie = db.get_or_404(Movie, movie_id)
+    db.session.delete(movie)
     db.session.commit()
-    return redirect(url_for('home'))
+    return redirect(url_for("home"))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
