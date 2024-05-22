6609c3959ead47bbe87c5258db7f472c5e952457
Tayaba Anwer <100337978+Tayyba27@users.noreply.github.com>
2024-05-22 10:13:38 +0500

    Add files via upload

--- Web_Develoment/main.py
+++ Web_Develoment/main.py
@@ -1,11 +1,16 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String, Boolean
-import random
+from sqlalchemy import Integer, String, Text
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditor, CKEditorField
+from datetime import date
 
 '''
-Install the required packages first: 
+Make sure the required packages are installed: 
 Open the Terminal in PyCharm (bottom left). 
 
 On Windows type:
@@ -14,119 +19,116 @@ python -m pip install -r requirements.txt
 On MacOS type:
 pip3 install -r requirements.txt
 
-This will install the packages from requirements.txt for this project.
+This will install the packages from the requirements.txt for this project.
 '''
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+ckeditor = CKEditor(app)
+Bootstrap5(app)
 
-# CREATE DB
+# CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
-
-# CREATE TABLE
-class Cafe(db.Model):
+# CONFIGURE TABLE
+class BlogPost(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    name: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
-    map_url: Mapped[str] = mapped_column(String(500), nullable=False)
-    img_url: Mapped[str] = mapped_column(String(500), nullable=False)
-    location: Mapped[str] = mapped_column(String(250), nullable=False)
-    seats: Mapped[str] = mapped_column(String(250), nullable=False)
-    has_toilet: Mapped[bool] = mapped_column(Boolean, nullable=False)
-    has_wifi: Mapped[bool] = mapped_column(Boolean, nullable=False)
-    has_sockets: Mapped[bool] = mapped_column(Boolean, nullable=False)
-    can_take_calls: Mapped[bool] = mapped_column(Boolean, nullable=False)
-    coffee_price: Mapped[str] = mapped_column(String(250), nullable=True)
-
-    def to_dict(self):
-        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    subtitle: Mapped[str] = mapped_column(String(250), nullable=False)
+    date: Mapped[str] = mapped_column(String(250), nullable=False)
+    body: Mapped[str] = mapped_column(Text, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
 
 
 with app.app_context():
     db.create_all()
 
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+# WTForm
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    author = StringField("Your Name", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
 
 
-@app.route("/random")
-def get_random_cafe():
-    result = db.session.execute(db.select(Cafe))
-    all_cafes = result.scalars().all()
-    random_cafe = random.choice(all_cafes)
-    return jsonify(cafe=random_cafe.to_dict())
+@app.route('/')
+def get_all_posts():
+    result = db.session.execute(db.select(BlogPost))
+    posts = result.scalars().all()
+    return render_template("index.html", all_posts=posts)
 
 
-@app.route("/all")
-def get_all_cafes():
-    result = db.session.execute(db.select(Cafe).order_by(Cafe.name))
-    all_cafes = result.scalars().all()
-    return jsonify(cafes=[cafe.to_dict() for cafe in all_cafes])
+@app.route("/post/<int:post_id>")
+def show_post(post_id):
+    requested_post = db.get_or_404(BlogPost, post_id)
+    return render_template("post.html", post=requested_post)
 
 
-@app.route("/search")
-def get_cafe_at_location():
-    query_location = request.args.get("loc")
-    result = db.session.execute(db.select(Cafe).where(Cafe.location == query_location))
-    # Note, this may get more than one cafe per location
-    all_cafes = result.scalars().all()
-    if all_cafes:
-        return jsonify(cafes=[cafe.to_dict() for cafe in all_cafes])
-    else:
-        return jsonify(error={"Not Found": "Sorry, we don't have a cafe at that location."}), 404
-
-# Test this inside Postman. Request type: Post ->  Body ->  x-www-form-urlencoded
-@app.route("/add", methods=["POST"])
-def post_new_cafe():
-    new_cafe = Cafe(
-        name=request.form.get("name"),
-        map_url=request.form.get("map_url"),
-        img_url=request.form.get("img_url"),
-        location=request.form.get("loc"),
-        has_sockets=bool(request.form.get("sockets")),
-        has_toilet=bool(request.form.get("toilet")),
-        has_wifi=bool(request.form.get("wifi")),
-        can_take_calls=bool(request.form.get("calls")),
-        seats=request.form.get("seats"),
-        coffee_price=request.form.get("coffee_price"),
-    )
-    db.session.add(new_cafe)
-    db.session.commit()
-    return jsonify(response={"success": "Successfully added the new cafe."})
-
-# Updating the price of a cafe based on a particular id:
-# http://127.0.0.1:5000/update-price/CAFE_ID?new_price=£5.67
-@app.route("/update-price/<int:cafe_id>", methods=["PATCH"])
-def patch_new_price(cafe_id):
-    new_price = request.args.get("new_price")
-    cafe = db.get_or_404(Cafe, cafe_id)
-    if cafe:
-        cafe.coffee_price = new_price
+@app.route("/new-post", methods=["GET", "POST"])
+def add_new_post():
+    form = CreatePostForm()
+    if form.validate_on_submit():
+        new_post = BlogPost(
+            title=form.title.data,
+            subtitle=form.subtitle.data,
+            body=form.body.data,
+            img_url=form.img_url.data,
+            author=form.author.data,
+            date=date.today().strftime("%B %d, %Y")
+        )
+        db.session.add(new_post)
         db.session.commit()
-        return jsonify(response={"success": "Successfully updated the price."}), 200
-    else:
-        return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."}), 404
-
-# Deletes a cafe with a particular id. Change the request type to "Delete" in Postman
-@app.route("/report-closed/<int:cafe_id>", methods=["DELETE"])
-def delete_cafe(cafe_id):
-    api_key = request.args.get("api-key")
-    if api_key == "TopSecretAPIKey":
-        cafe = db.get_or_404(Cafe, cafe_id)
-        if cafe:
-            db.session.delete(cafe)
-            db.session.commit()
-            return jsonify(response={"success": "Successfully deleted the cafe from the database."}), 200
-        else:
-            return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."}), 404
-    else:
-        return jsonify(error={"Forbidden": "Sorry, that's not allowed. Make sure you have the correct api_key."}), 403
+        return redirect(url_for("get_all_posts"))
+    return render_template("make-post.html", form=form)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
+def edit_post(post_id):
+    post = db.get_or_404(BlogPost, post_id)
+    edit_form = CreatePostForm(
+        title=post.title,
+        subtitle=post.subtitle,
+        img_url=post.img_url,
+        author=post.author,
+        body=post.body
+    )
+    if edit_form.validate_on_submit():
+        post.title = edit_form.title.data
+        post.subtitle = edit_form.subtitle.data
+        post.img_url = edit_form.img_url.data
+        post.author = edit_form.author.data
+        post.body = edit_form.body.data
+        db.session.commit()
+        return redirect(url_for("show_post", post_id=post.id))
+    return render_template("make-post.html", form=edit_form, is_edit=True)
+
+
+@app.route("/delete/<int:post_id>")
+def delete_post(post_id):
+    post_to_delete = db.get_or_404(BlogPost, post_id)
+    db.session.delete(post_to_delete)
+    db.session.commit()
+    return redirect(url_for('get_all_posts'))
+
+
+# Code from previous day
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5002)
